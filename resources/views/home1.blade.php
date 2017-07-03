aa2c6d1aa30966143b6756ec1e241bedd7374e4c
yuzesun <yuzesun@unomaha.edu>
2017-07-03 12:53:59 -0500

    testing

---
+++ resources/views/home1.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuzes
+ * Date: 6/27/2017
+ * Time: 10:52 AM
+ */
---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuzes
+ * Date: 5/15/2017
+ * Time: 12:28 PM
+ */
